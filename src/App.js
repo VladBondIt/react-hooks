24f7a39baff4ea3f2d0bf1621feb8a3b3c4ce242
Vlad Bond <foolowin@gmail.com>
2020-11-25 13:22:50 +0300

    pactice-solo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
-function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  // addCount() {
+  //   this.setState(){
+  //     this.count++
+  //   }
+  // }
+
+  render() {
+    const { name } = this.props;
+
+    return (
+      <>
+        <h1>Hello world {name}!</h1>
+        <div>{this.state.count}</div>
+        <button>Click me plz</button>
+      </>
+    )
+  }
 
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Vlad" />
   </React.StrictMode>,
   document.getElementById('root')
 );
