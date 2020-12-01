5cb3aa5e95ce20b0fc7bb52ddc3bce5e7e5b661c
Vlad Bond <foolowin@gmail.com>
2020-12-01 16:32:13 +0300

    redux-begin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ReactHooks from './Hooks/React-hooks';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import ReactHooks from './Hooks/React-hooks';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ReactHooks />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ReactHooks />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const initialState = 0;
+
+// Action всегда объект,он должен всегда сожержать ключ type 
+const reducer = (state, action) => {
+
+
+}
+
+let state = reducer(initialState, { type: 'INC' })
