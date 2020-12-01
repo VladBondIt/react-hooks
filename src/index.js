aa63ace057757e5a8e4482eabe0e78e09cf76f00
Vlad Bond <foolowin@gmail.com>
2020-12-01 17:00:01 +0300

    tbc

--- src/index.js
+++ src/index.js
@@ -10,12 +10,21 @@
 //   document.getElementById('root')
 // );
 
+// Глобал объект Редюсер призван решать проблемы возникающие при 
+// вынесении Стейта в глобальное пространство в котором доступен для всех компонентов приложения.
+
 const initialState = 0;
 
-// Action всегда объект,он должен всегда сожержать ключ type 
+// Action всегда объект,он должен всегда сожержать ключ type и значение должно быть всегда строкой.
 const reducer = (state, action) => {
-
+  if (action.type === 'INC') {
+    return state + 1;
+  }
 
 }
 
-let state = reducer(initialState, { type: 'INC' })
+let state = reducer(initialState, { type: 'INC' });
+
+console.log(state);
+state = reducer(state, { type: 'INC' });
+console.log(state);
